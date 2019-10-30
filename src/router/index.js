e10eaec0723a6937c239002e07c5099bc96ed5cf
maccydrums <marcusprivat@live.com>
2019-10-30 16:15:04 +0100

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,24 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Login from '../views/Login.vue'
+import ChatDashboard from '../views/ChatDashboard.vue'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/chat',
+    name: 'chat',
+    component: ChatDashboard,
   }
 ]
 
-const router = new VueRouter({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
